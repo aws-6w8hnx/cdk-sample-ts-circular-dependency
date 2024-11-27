d6b03a6a39df1dd2aa7a49be278ba46846c1f4e3
Qiong Fan <jfan@FQs-Mac-server.local>
2024-11-27 16:01:05 +1300

    Initial commit

---
+++ test/circular-dependency.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CircularDependency from '../lib/circular-dependency-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/circular-dependency-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CircularDependency.CircularDependencyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
